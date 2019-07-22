46c5946d335d08c93f7a284ba7ff03eb40f739d6
Jeffrey Matos <matos.jeffrey.j@gmail.com>
2019-07-22 16:37:40 -0400

    Development Mode Error

--- SevensPizza/Startup.cs
+++ SevensPizza/Startup.cs
@@ -45,15 +45,17 @@ namespace SevensPizza
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                app.UseHsts();
-            }
+            app.UseDeveloperExceptionPage();
+            app.UseDatabaseErrorPage();
+            //if () //env.IsDevelopment()
+            //{
+                
+            //}
+            //else
+            //{
+            //    //app.UseExceptionHandler("/Home/Error");
+            //    //app.UseHsts();
+            //}
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
